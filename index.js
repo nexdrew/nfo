b9ad320d7a0cb64a9c1c2bd19c4c089c0505d967
Andrew Goode <andrewbgoode@gmail.com>
2017-10-06 13:53:48 -0700

    perf: improved performance via concurrent promises (#4)

--- index.js
+++ index.js
@@ -2,25 +2,20 @@
 const cp = require('child_process')
 
 module.exports = function nfo (packages, field) {
-  const pkgs = [].concat(packages)
-  const results = {}
-  function next () {
-    return pkgs.length === 0
-      ? Promise.resolve(results)
-      : aggregate(pkgs.splice(0, 1)[0], field, results).then(next)
-  }
-  return next()
-}
-
-function aggregate (pkg, field, agg) {
-  return npmInfo(pkg, field).then(result => {
-    agg[result.pkg] = { field: result.field }
-    try {
-      eval('agg[result.pkg].value = ' + String(result.stdout).trim()) // eslint-disable-line no-eval
-    } catch (_) {
-      agg[result.pkg].value = String(result.stdout).trim()
-    }
-    return agg
+  const promises = []
+  ;[].concat(packages).forEach(pkg => {
+    promises.push(npmInfo(pkg, field))
+  })
+  return Promise.all(promises).then(results => {
+    return results.reduce((agg, result) => {
+      agg[result.pkg] = { field: result.field }
+      try {
+        eval('agg[result.pkg].value = ' + String(result.stdout).trim()) // eslint-disable-line no-eval
+      } catch (_) {
+        agg[result.pkg].value = String(result.stdout).trim()
+      }
+      return agg
+    }, {})
   })
 }
 
